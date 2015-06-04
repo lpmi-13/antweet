67fbf79430403c05cffd6316c8fef1ded23b4bd4
lpmi-13 <leskis@gmail.com>
2015-06-04 13:50:25 +0100

    fixed function in app.js

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var express = require('express'),
 	stylus = require('stylus'),
 	nib = require('nib'),
 	twit = require('twit'),
-	app = express()
+	app = express();
+	
 	function compile(str, path) {
 		return stylus(str)
 		.set('filename', path)
 		.use(nib());
-	};
+	}
 
 //setting jade templates
 app.set('views', './views');
