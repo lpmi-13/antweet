ee4add0b2e603a39ba7dcc3cc6ad8eb0628f4870
lpmi-13 <leskis@gmail.com>
2015-06-04 13:54:47 +0100

    fixed favicon

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 var express = require('express'),
 	bodyparser = require('body-parser'),
 	path = require('path'),
+	favicon = require('serve-favicon');
 	stylus = require('stylus'),
 	nib = require('nib'),
 	twit = require('twit'),
 	app = express();
-	
+
 	function compile(str, path) {
 		return stylus(str)
 		.set('filename', path)
@@ -22,9 +23,9 @@ app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(stylus.middleware(
-	{ src: __dirname + 'public',
-	compile: compile
-	}
+  { src: __dirname + '/public',
+  compile: compile
+  }
 ));
 
 
