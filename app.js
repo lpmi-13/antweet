50699f9c0ff72f4d7b4f1c38386bb44be112c5f8
lpmi-13 <leskis@gmail.com>
2015-06-07 20:05:30 +0100

    reworked jade

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ var express = require('express'),
 	bodyparser = require('body-parser'),
 	path = require('path'),
 	favicon = require('serve-favicon'),
-	stylus = require('stylus'),
-	nib = require('nib'),
 	twit = require('twit'),
 	app = express();
 
-	function compile(str, path) {
-		return stylus(str)
-		.set('filename', path)
-		.use(nib());
-	}
 
 //setting jade templates
 app.set('views', './views');
@@ -22,12 +15,6 @@ app.set('view engine', 'jade');
 //setting...ummm, the app, to...ummm...use something...?
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyparser.urlencoded({extended: false}));
-app.use(stylus.middleware(
-  { src: __dirname + '/public',
-  compile: compile
-  }
-));
-
 
 //storing API key variables to be accessed via heroku processes
 var T = new twit({
@@ -46,11 +33,11 @@ app.post('/', function(req, res) {
 	var tweet_top = req.body.str;
 
 	function findTweet() {
-	T.get('search/tweets', {q: tweet_top, count : 1 }, function (err, data, response) {
+	T.get('search/tweets', {q: ['the', 'a', 'an', tweet_top], count : 5 }, function (err, data, response) {
 			var topd = data.statuses.map(function(t){
 			return t.text;
 		});
-		res.render('result', {string: 'the tweet is ' + topd});
+		res.render('result', {string: topd});
 	});
 }
 	findTweet();
