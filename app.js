9250920f0021dc415cb3fafdffc9f6a5b108571b
lpmi-13 <leskis@gmail.com>
2015-07-15 22:28:29 +0100

    remove RTs

--- app.js
+++ app.js
@@ -34,10 +34,11 @@ app.post('/', function(req, res) {
 	var tweet_top = req.body.str;
 
 	function findTweet() {
-	T.get('search/tweets', {q: ['the', 'a', 'an', tweet_top], count : 5 }, function (err, data, response) {
+	T.get('search/tweets', {q: ['the', 'a', 'an', tweet_top], include_rts: false, count : 5 }, function (err, data, response) {
 			var topd = data.statuses.map(function(t){
 			return t.text;
 		});
+
 		res.render('result', {string: topd});
 	});
 }
