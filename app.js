a72a5af41182d4bfe143205637e56b90ed85fbef
lpmi-13 <leskis@gmail.com>
2016-05-30 16:41:43 +0100

    with new styling

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var T = new twit({
 	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
 	access_token: process.env.ACCESS_TOKEN,
-	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+	access_token_secret: process.env.TOKEN_SECRET
 });
 
 app.get('/', function(req, res) {
