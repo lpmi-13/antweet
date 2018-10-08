287e465c80897853f835b0e4aaa7a461ec7b6374
lpmi-13 <leskis@gmail.com>
2018-10-08 08:14:56 +0100

    update formatting with prettier

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-
 //module dependencies
 var express = require('express'),
-	bodyparser = require('body-parser'),
-	path = require('path'),
-	favicon = require('serve-favicon'),
-	twit = require('twit'),
-	app = express();
-
+  bodyparser = require('body-parser'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  twit = require('twit'),
+  app = express();
 
 //setting jade templates
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static(__dirname, 'public'));
 
 //storing API key variables to be accessed via heroku processes
 var T = new twit({
-	consumer_key: process.env.CONSUMER_KEY,
-	consumer_secret: process.env.CONSUMER_SECRET,
-	access_token: process.env.ACCESS_TOKEN,
-	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 app.get('/', function(req, res) {
-	res.render('index', {"query": req.query.string});
+  res.render('index', { query: req.query.string });
 });
 
 app.post('/', function(req, res) {
+  var tweet_top = req.body.str;
 
-	var tweet_top = req.body.str;
+  function findTweet() {
+    T.get('search/tweets', { q: ['the', 'a', 'an', tweet_top, 'exclude:retweets'], count: 5 }, function(
+      err,
+      data,
+      response
+    ) {
+      var topd = data.statuses.map(function(t) {
+        return t.text;
+      });
 
-	function findTweet() {
-	T.get('search/tweets', {q: ['the', 'a', 'an', tweet_top, 'exclude:retweets'], count : 5 }, function (err, data, response) {
-			var topd = data.statuses.map(function(t){
-				return t.text;
-		});
-
-		res.render('result', {string: topd});
-	});
-}
-	findTweet();
+      res.render('result', { string: topd });
+    });
+  }
+  findTweet();
 });
 
 app.listen(process.env.PORT || 3001);
